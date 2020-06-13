6b2c26082719f95d9bfa31ec7808c19402d793c1
Vaishnavi Deshpande <vaishdesh2001@gmail.com>
2020-06-12 21:10:07 -0500

    latest commit

--- flask_climate.py
+++ flask_climate.py
@@ -2,6 +2,8 @@ from flask import Flask, render_template, flash
 from forms import Get_Plot
 import Climate_Runner_App
 
+#web app runner
+
 app = Flask(__name__)
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
